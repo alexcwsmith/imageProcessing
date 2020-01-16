8774c797927ce1ad5a378d07c9a1b2471449c9f3
smith <alexaner.smith@mssm.edu>
2020-01-16 14:07:32 -0500

    Updated Details

--- clearMapStackCropping.py
+++ clearMapStackCropping.py
@@ -6,13 +6,17 @@ Created on Sun Dec 15 15:08:02 2020
 @author: smith
 
 This script finds the edges of the TIF stack and crops them to be +/- 50 pixels from those edges.
-
+It writes a .xlsx file with the new coordinates in ImageJ format. While it is possible to change the threshold values established here,
+because of greater variability in signal than autofluorescence, it is best to use this script on the autofluorescence file,
+then manually crop the data image stack with info from the .xlsx output (using Imagej --> edit --> selection --> specify)
 
 """
 
 import os
 import numpy as np
+import pandas as pd
 import skimage
+
 
 sampleID = 'ROC_13'
 baseDirectory = '/d2/studies/ClearMap/ROC_iDISCO/' + sampleID
@@ -20,7 +24,7 @@ data = skimage.io.imread(os.path.join(baseDirectory, sampleID + '_auto_stack.ome
 
 X1 = 500
 X2 = 1500
-XminData = data[600:700,1250:1550,:X1] #note dimension indices are [z,y,x], these need to contain the widest part of your brain
+XminData = data[600:700,1250:1550,:X1] #note dimension indices are [z,y,x], these need to contain the widest part of your brain, but should be limited to reduce computation
 XmaxData = data[600:700,1250:1550,X2:]
 Xmin = np.nonzero(XminData[2]>2000)[1]
 Xmin = (Xmin.min()-50)
@@ -38,6 +42,17 @@ Ymax = np.nonzero(YmaxData[1]<500)[0]
 Ymax = ((Ymax.min())+50)
 Ymax = Ymax + Y2
 
+Width = (Xmax-Xmin).astype(int)
+Height = Ymax-Ymin.astype(int)
+
+if Width < 1500:
+    raise ValueError('Small width dimension detected, proceed with caution.')
+if Height < 1500:
+    raise ValueError('Small height dimension detected, proceed with caution.')
+else: print(Width, Height)
+   
+Coords = pd.DataFrame(data=[Width,Height,Xmin,Ymin], index=['Width', 'Height', 'Xmin', 'Ymin'], columns=['Coordinate(pixel)'])
+Coords.to_excel(os.path.join(baseDirectory, 'Cropping_coordinates.xlsx'))
 
 dataCrop = data[:data.shape[0],Ymin:Ymax,Xmin:Xmax,]
 skimage.io.imsave(os.path.join(baseDirectory, sampleID + '_auto_cropped.ome.tif'), dataCrop)
