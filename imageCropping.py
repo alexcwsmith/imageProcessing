877b6bf193324abdf7d8f31038ae42bba9d6df4f
Alex Smith <alexander.smith@mssm.edu>
2020-01-27 19:01:11 -0500

    Updated

--- imageCropping.py
+++ imageCropping.py
@@ -12,10 +12,9 @@ import skimage
 import glob
 from skimage.external import tifffile
 
-sampleID = 'N1_subset_test_auto'
-
-path = '/d2/studies/ClearMap/Molly/N1_subset_test/auto/'
-resultpath = os.path.join(path, 'skimage_rescaled/')
+sampleID = 'ROC_1'
+path = '/d2/studies/ClearMap/ROC_iDISCO/' + sampleID + '/SPmR/'
+resultpath = os.path.join(path, 'SPmR_cropped/')
 if not os.path.exists(resultpath):
     os.mkdir(resultpath)    
 dirs = os.listdir(path)
@@ -25,15 +24,15 @@ def cropAndResample():
         fullpath = os.path.join(path,item)
         if os.path.isfile(fullpath):
             im = skimage.io.imread(fullpath)
-            imCrop = im[32:8736,833:6959] #replaced 1008:7040, [32:8736,1008:6784]
+            imCrop = im[Ymin:Ymax,Xmin:Xmax] #replaced 1008:7040, [32:8736,1008:6784]
             filename = os.path.basename(fullpath)
             f, e = os.path.splitext(filename)
-            rescaled = skimage.transform.rescale(imCrop, 0.5, anti_aliasing=False)
-            rescaled16 = skimage.img_as_uint(rescaled)
-            skimage.io.imsave(os.path.join(resultpath, f + '_skimage_rescaled.tif'), rescaled16)
-    with tifffile.TiffWriter(path + sampleID + '.ome.tif', bigtiff=True, imagej=True) as stack:
-        for filename in sorted(glob.glob(os.path.join(resultpath, '*.tif'))):
-            stack.save(tifffile.imread(filename), photometric='minisblack')
+#            rescaled = skimage.transform.rescale(imCrop, 0.5, anti_aliasing=False)
+#            rescaled16 = skimage.img_as_uint(rescaled)
+            skimage.io.imsave(os.path.join(resultpath, f + '_cropped.tif'), imCrop)
+#    with tifffile.TiffWriter(path + sampleID + '.ome.tif', bigtiff=True, imagej=True) as stack:
+#        for filename in sorted(glob.glob(os.path.join(resultpath, '*.tif'))):
+#            stack.save(tifffile.imread(filename), photometric='minisblack')
 
 cropAndResample()
 
