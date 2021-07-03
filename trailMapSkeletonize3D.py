54bafb149e25af4bc497b6c698fa6682d9e1e491
Alex Smith <alexander.smith@mssm.edu>
2021-07-03 13:27:02 -0400

    Added debugging and 16bit conversion options

--- trailMapSkeletonize3D.py
+++ trailMapSkeletonize3D.py
@@ -7,61 +7,88 @@ Created on Fri Sep 18 14:14:35 2020
 """
 
 import os
+import skimage
 from skimage import io
+from skimage import img_as_uint
 from skimage import morphology
 import numpy as np
+from scipy import ndimage
 import time
+import argparse
 
-directory='/d2/studies/ClearMap/FosTRAP_ChR2_March2021/'
-sample='F3_RT'
-crop = {'x':110,'y':140,'width':1900,'height':2400}
+directory='/d2/studies/ClearMap/ROC_ChR2/ROC31/seg-ChR2/'
+if not os.path.exists(directory):
+    raise FileNotFoundError("Directory does not exist, check path")
+#ROC33: crop = {'width':1900,'height':2300,'x':170,'y':260}
+crop={'width':1900,'height':2300,'x':135,'y':260} #ROC31
 
-def skel(directory, sample, crop=None, flip=None):
-    """Threshold and skeletonize 3D axons as described in Friedmann et al., PNAS 2020 (PMID: 32358193).
+def skel(directory, crop=None, flip='y', convertTo16Bit=False, debug=False):
+    """Skeletonize TrailMap results.
     
     Parameters
     ----------
-    directory : str
-        Path to base directory containing all sample subdirectories.
-    sample : str
-        Name of sample (must be name of subdirectory in directory).
+    directory : string
+        Path to directory with segmented data.
     crop : dict (optional, default None)
-        Dictionary of {x:int, y:int, width:int, height:int} to give cropping coordinates (ImageJ selection format).
-    flip : str (optional, default None)
-        Axis to flip/invert after completing. Valid arguments are 'x', 'y', 'z'.
-        
-    Returns
-    -------
-    None.
+        Dictionary with ImageJ-format cropping coordinates ({width:, height:, x:, y:,})
+    flip : string (optional, default 'y')
+        Option to flip axis, can be any combination of 'xyz'.
+    convertTo16Bit : bool
+        Whether to convert the image to 16 bit before saving. Have not double checked that this gives the same results as converting in ImageJ yet though.
+    debug : bool
+        Run in debug mode, process only a small substack of data, 50 planes in the center of the stack.
     """
-    print("Started " + sample + ' ' + time.ctime())
-    path = os.path.join(directory, sample + '/seg-ChR2/')
-    ims = io.ImageCollection(path+'*.ome.tif', load_func=io.imread)
+    sample = os.path.realpath(directory).split('/')[-2] #the number at the end of this line may change based on the file path of directory, for the current path -2 is correct (sample variable is ROC_9)"
+    print("Started " + str(sample) + " at " + time.ctime())
+    ims = io.ImageCollection(load_pattern=os.path.join(directory, '*.tif'), load_func=io.imread)
     data = ims.concatenate()
     if crop:
         rawshape=data.shape
-        x=crop['x']
-        y=crop['y']
-        width=crop['width']
-        height=crop['height']
-        data = data[:,y:y+height,x:x+width]
+        data = data[:,crop['y']:crop['y']+crop['height'],crop['x']:crop['x']+crop['width']]
         print("Cropped data from " + str(rawshape) + " to " + str(data.shape) + " at " + time.ctime())
+    if debug:
+        center = data.shape[0]//2
+        data = data[center-25:center+25,:,:]
     cat = np.zeros(shape=(data.shape), dtype='float32')
     for i in range(2,10,1):
+        print(str(i) + " started at " + time.ctime())
         i=i/10
         im = (data>i).astype('float32')
         skel = morphology.skeletonize_3d(im).astype('float32')*i
         print(str(i) + " completed at " + time.ctime())
         cat = cat+skel
-    if flip=='y':
-        cat = np.flip(cat, axis=1)
-    elif flip=='x':
-        cat = np.flip(cat, axis=2)
-    elif flip=='z':
-        cat = np.flip(cat, axis=0)
-    io.imsave(os.path.join(directory, sample + '/' + sample + '_ThresholdedSkeleton3D.tif'), cat, check_contrast=False)
+    if flip:
+        if 'y' in flip:
+            cat = np.flip(cat, axis=1)
+        if 'x' in flip:
+            cat = np.flip(cat, axis=2)
+        if 'z' in flip:
+            cat = np.flip(cat, axis=0)
+    if not convertTo16Bit and not debug:
+        io.imsave(os.path.join(directory, sample + '_ThresholdedSkeleton3D.tif'), cat, check_contrast=False)
+    elif not convertTo16Bit and debug:
+        io.imsave(os.path.join(directory, sample + '_ThresholdedSkeleton3D_DEBUG.tif'), cat, check_contrast=False)
+    elif convertTo16Bit and not debug:
+        cat = cat/cat.max()
+        cat = img_as_uint(cat)
+        io.imsave(os.path.join(directory, sample+ '_ThresholdedSkeleton3D_16bit.tif'), cat, check_contrast=False)
+    elif convertTo16Bit and debug:
+        cat = cat/cat.max()
+        cat = img_as_uint(cat)
+        io.imsave(os.path.join(directory, sample + '_ThresholdedSkeleton3D_16bit_DEBUG.tif'), cat, check_contrast=False)
     print("Finished " + sample + ' ' + time.ctime())
+    return cat
 
-skel(directory, sample, crop, flip='y')
+cat = skel(directory, crop=crop, flip='y', convertTo16Bit=True, debug=False)
 
-
+if __name__ == '__main__':
+    p = argparse.ArgumentParser()
+    p.add_argument('--directory',type=str,default=os.getcwd(),help='Directory with segmented images.')
+    p.add_argument('--crop',type=dict,default=None,help='Dict with ImageJ-format cropping coordinates.')
+    p.add_argument('--flip',type=str,default=None,help='xyz axes to flip')
+    p.add_argument('--convertTo16Bit',type=bool,default=False,help='Whether to convert to 16 bit image before saving')
+    p.add_argument('--debug',type=bool,default=False,help='If True, only process 50 planes from center of the stack for faster debugging')
+    args = p.parse_args()
+    skel(args.directory, crop=args.crop, flip=args.flip, convertTo16Bit=args.convertTo16Bit, debug=args.debug)
+    
+    
